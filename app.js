123e8da66fd74c47dcb73c234caca78edc42d27d
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-10-13 17:49:43 -0700

    fixed app so it doesn't produce role as extra info and rewrote code so it prompts manager 1st

--- app.js
+++ app.js
@@ -12,78 +12,113 @@ const render = require("./lib/htmlRenderer");
 
 //array where everyone is stored
 const folks = [];
+bossFacts();
+function bossFacts(){
+    inquirer
+        .prompt([{
+            type: "input",
+            message: "What is your name?",
+            name: "name",
+        },{
+            type: "number",
+            message: "What is your ID?",
+            name: "id",
+        },{
+            type: "input",
+            message: "What is your email address?",
+            name: "email",
+        },{
+            type: "input",
+            message: "What is your office number?",
+            name: "officeNumber",
+            validate: async (input) => {
+                if (isNaN(input)) {
+                    return "Please enter a number";
+                }
+                return true;
+            }
+        }
+    ]).then(function({name,id,email,officeNumber}) {
+            var role = "Manager";
+            const manager = new Manager(name, id, email, officeNumber);
+            folks.push(manager);
+            console.log(`You said your name was ${name} and that you are the manager`);
+            doneYet();
+        })
 
-//function asks the questions
-//to do: re-order and ask manager and them for employee info
+
+}
+
 function askQuestions() {
     inquirer
     .prompt([{
         type: "input",
-        message: "What is your name?",
+        message: "What is your employee's name?",
         name: "name",
     },
     {
         type: "number",
-        message: "What is your ID?",
+        message: "What is your employee's ID?",
         name: "id",
     }, {
         type: "input",
-        message: "What is your email address?",
+        message: "What is your employee's email address?",
         name: "email",
     },
     {
         type: "list",
-        message: "What is your role?",
+        message: "What is your employee's role?",
         name: "role",
-        choices: ["Engineer", "Intern", "Manager"]
+        choices: ["Engineer", "Intern"]
     }
     // roles
 ]).then(
-    function({ name, id, email, role }) {
-        if ( role == "Engineer"){ 
-            inquirer
+    function({ name, id, email, role}) {
+        if ( role === "Engineer"){ inquirer
                 .prompt({
                     type: "input",
                     message: "What is your GitHub username?",
-                    name: "github"
+                    name: "github",
+                    validate: async (input) => {
+                        if (input == "" || /\s/.test(input)) {
+                            return "Please enter a valid GitHub username";
+                        }
+                        return true;
+                    }
+
                     }).then( function({github}) {
-                        const eng = new Engineer(name, id, email, role, github);
-                        folks.push(eng);
-                        console.log(folks);
-                        doneYet();
+                
+                    const engineer = new Engineer(name, id, email, github);
+                    folks.push(engineer);
+                    console.log(`Added an Engineer by the name of ${name}, with an employee id of ${id}, an email address of ${email}, and a github of ${github}`);
+                    doneYet();
                     })
 
         } else if ( role == "Intern"){ inquirer
             .prompt({
                 type: "input",
-                message: "What school do you attend?",
-                name: "school"
+                message: "What school did/does your intern attend?",
+                name: "school",
+                validate: async (input) => {
+                    if (input == "") {
+                        return "Please enter a name.";
+                    }
+                    return true;
+                }
                 
             }).then( function({school}) {
                 const intern = new Intern(name, id, email, role, school);
                 folks.push(intern);
-                console.log(folks);
+                console.log(`Added an Intern by the name of ${name}, with an employee id of ${id}, an email address of ${email}, and that goes/went to ${school}`);
                 doneYet();
             })
 
 
-        } else if (role == "Manager") {
-            inquirer
-            .prompt({
-                type: "input",
-                message: "What is your Office Number?",
-                name: "officeNumber"
-            }).then( function({officeNumber}) {
-               const manager = new Manager(name, id, email, role, officeNumber);
-                folks.push(manager);
-                console.log(folks);
-                doneYet();
-            })
-        }
+        } 
     })};
 
 
-askQuestions();
+// askQuestions();
 
 
 function doneYet(){
@@ -96,11 +131,12 @@ function doneYet(){
     }]).then(
         function({moreFolks}) {
             if ( moreFolks == "Yes"){ 
+                // askQuestions();
                 askQuestions();
             }else{ const results = render(folks);
                      fs.writeFile("./output/team.html", results, function(err) { 
                              if(err) throw err;
-                          });console.log("Successfully wrote html");
+                          });console.log("Successfully wrote html, now you can open it and see how it looks!");
                     
                     }
         })}
